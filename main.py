09377d49524b695d4422d8a9b99f594c5845f5be
milankatira <milankatira26@gmail.com>
2026-05-30 14:37:17 +0530

    Add support for ChatOllama in main.py and update dependencies
    - Integrated dotenv for environment variable management.
    - Implemented a test call to ChatOllama to retrieve the capital of France.
    - Added langchain-ollama as a dependency in pyproject.toml.
    - Updated uv.lock to include langchain-ollama and its dependencies.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from prod-rag!")
+from dotenv import load_dotenv
 
+load_dotenv()
+
+from langchain_ollama import ChatOllama
+def main():
+
+    # testing the llm
+    llm = ChatOllama(model="gemma4")
+    response = llm.invoke("What is the capital of France?")
+    print(response.content)
 
 if __name__ == "__main__":
     main()
